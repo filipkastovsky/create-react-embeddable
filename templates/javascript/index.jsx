fadfb491101e645062dfc70523768b68477eb362
filipkastovsky <devfilipkastovsky@gmail.com>
2020-05-29 23:16:07 +0200

    JS template added

---
+++ templates/javascript/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { App } from './src/App';
+import { withObservableAttrs } from './src/utils/withObservableAttrs';
+import { ROOT_ID } from './src/config/Config';
+
+const root = document.getElementById(ROOT_ID);
+
+const AppWithObservableAttrs = withObservableAttrs(root, App);
+
+ReactDOM.render(<AppWithObservableAttrs />, root);
